3d9042d7fab7e319163440905d018449708aa298
Max Weiner <max.weiner@posteo.de>
2024-02-04 12:21:10 +0100

    add sorting

--- src/pyskat/cli/evaluate_commands.py
+++ src/pyskat/cli/evaluate_commands.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import click
 
 from .main import pass_backend
@@ -11,24 +13,57 @@ def evaluate():
 
 
 @evaluate.command()
+@click.option(
+    "-s", "--sort-by",
+    type=click.STRING,
+    default=None,
+    help="Column key to sort results by."
+)
+@click.option(
+    "-r", "--reverse",
+    type=click.BOOL,
+    default=False,
+    is_flag=True,
+    help="Sort in reverse order."
+)
 @pass_backend
-def results(backend: Backend):
-    evaluation = backend.evaluate_results()
+def results(backend: Backend, sort_by: Optional[str], reverse: bool):
+    try:
+        evaluation = backend.evaluate_results()
 
-    print_pandas_dataframe(evaluation)
+        print_pandas_dataframe(evaluation.sort_values(sort_by, ascending=reverse))
+    except KeyError:
+        console.print_exception()
 
 
 @evaluate.command()
+@click.option(
+    "-s", "--sort-by",
+    type=click.STRING,
+    default=None,
+    help="Column key to sort results by."
+)
+@click.option(
+    "-r", "--reverse",
+    type=click.BOOL,
+    default=False,
+    is_flag=True,
+    help="Sort in reverse order."
+)
 @pass_backend
-def total(backend: Backend):
-    evaluation = backend.evaluate_total()
+def total(backend: Backend, sort_by: Optional[str], reverse: bool):
+    try:
+        evaluation = backend.evaluate_total()
 
-    for ind in evaluation.columns.levels[0]:
-        if isinstance(ind, int):
-            title = f"Series {ind}"
-        elif isinstance(ind, str):
-            title = ind.title()
-        else:
-            title = None
+        for ind in evaluation.columns.levels[0]:
+            if isinstance(ind, int):
+                title = f"Series {ind}"
+            elif isinstance(ind, str):
+                title = ind.title()
+            else:
+                title = None
 
-        print_pandas_dataframe(evaluation[ind], title)
+            print_pandas_dataframe(evaluation[ind].sort_values(sort_by, ascending=reverse), title)
+            console.print()
+    except KeyError:
+        console.print_exception()
