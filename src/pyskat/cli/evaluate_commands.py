4d3d213fae2402f494e9243b1060883baa2fb569
Max Weiner <max.weiner@posteo.de>
2024-02-04 12:25:23 +0100

    add docstrings

--- src/pyskat/cli/evaluate_commands.py
+++ src/pyskat/cli/evaluate_commands.py
@@ -9,6 +9,7 @@ from ..rich import console, print_pandas_dataframe
 
 @click.group()
 def evaluate():
+    """Evaluate game results."""
     pass
 
 
@@ -28,6 +29,7 @@ def evaluate():
 )
 @pass_backend
 def results(backend: Backend, sort_by: Optional[str], reverse: bool):
+    """Evaluate and display all game results."""
     try:
         evaluation = backend.evaluate_results()
 
@@ -52,6 +54,7 @@ def results(backend: Backend, sort_by: Optional[str], reverse: bool):
 )
 @pass_backend
 def total(backend: Backend, sort_by: Optional[str], reverse: bool):
+    """Evaluate and display game results per series and in total."""
     try:
         evaluation = backend.evaluate_total()
 
