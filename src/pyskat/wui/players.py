4080ecd2af81ca64288f6d4bfb143019cefe3dcb
Max Weiner <max.weiner@posteo.de>
2024-08-11 22:56:45 +0200

    make players editable in table

--- src/pyskat/wui/players.py
+++ src/pyskat/wui/players.py
@@ -1,46 +1,82 @@
+from pydantic import ValidationError
+
 from .app import get_nav_items
-from flask import render_template, g, request, Blueprint
+from flask import render_template, g, request, Blueprint, abort, redirect, url_for, flash
 
 bp = Blueprint("players", __name__, url_prefix="/players")
 
 
 @bp.route("/", methods=["GET", "POST"])
-def players():
-    edit_id = request.args.get("edit", None)
-
-    if request.method == "POST":
-        if edit_id:
-            g.backend.players.update(
-                id=int(edit_id),
-                name=request.form["name"],
-                remarks=request.form["remarks"],
-            )
-        else:
-            g.backend.players.update(
-                name=request.form["name"],
-                remarks=request.form["remarks"],
-            )
-
-    if edit_id:
-        try:
-            edit_player = g.backend.players.get(edit_id)
-        except KeyError:
-            edit_player = None
-    else:
-        edit_player = None
-
-    remove_id = request.args.get("remove", None)
-    if remove_id:
-        try:
-            g.backend.players.remove(int(remove_id))
-        except KeyError:
-            pass
-
+def index():
     players_list = g.backend.players.all()
 
     return render_template(
         "players.html",
         nav_items=get_nav_items(active="players"),
         players=players_list,
-        edit_player=edit_player,
     )
+
+
+@bp.post("/add")
+def add():
+    try:
+        g.backend.players.add(
+            name=request.form["name"],
+            active=request.form["active"],
+            remarks=request.form["remarks"],
+        )
+    except ValidationError as e:
+        flash_validation_error(e)
+
+    return redirect_to_index()
+
+
+@bp.post("/update/<int:id>")
+def update(id: int):
+    try:
+        name = request.form["name"]
+        remarks = request.form["remarks"]
+        active = request.form.get("active", False, bool)
+    except KeyError:
+        abort(400, description="Invalid form data submitted.")
+
+    try:
+        g.backend.players.update(
+            id=id,
+            name=name,
+            active=active,
+            remarks=remarks,
+        )
+    except KeyError:
+        flash_player_not_found(id)
+    except ValidationError as e:
+        flash_validation_error(e)
+
+    return redirect_to_index()
+
+
+@bp.post("/remove/<int:id>")
+def remove(id: int):
+    try:
+        g.backend.players.remove(id)
+    except KeyError:
+        flash_player_not_found(id)
+    return redirect_to_index()
+
+
+def flash_player_not_found(id: int):
+    flash(f"Player {id} not found.", "danger")
+
+
+def redirect_to_index():
+    return redirect(url_for("players.index"))
+
+
+def flash_validation_error(error: ValidationError):
+    validation_messages = [format_validation_message(e) for e in error.errors()]
+    flash("Submitted data was invalid:\n\n" + "\n".join(validation_messages))
+
+
+def format_validation_message(e: dict):
+    loc = ", ".join(e["loc"])
+    return f"{loc}: {e['msg']}"
