acc3cd4359885c183ba15562565a760e1ea1b277
Max Weiner <max.weiner@posteo.de>
2024-09-08 09:06:22 +0200

    add first plots

--- src/pyskat/plugins/plot.py
+++
@@ -1,8 +0,0 @@
-from ..backend import Backend
-from .manager import plugin_manager
-import plotly.graph_objects as go
-import pandas as pd
-
-def create_result_plots(backend: Backend, results: pd.DataFrame) -> list[go.Figure]:
-    plots = plugin_manager.hook.plot_results(backend=backend, results=results)
-    return plots
--- src/pyskat/wui/evaluation.py
+++ src/pyskat/wui/evaluation.py
@@ -1,18 +1,13 @@
-import numpy as np
-from pydantic import ValidationError
-
-from .helpers import flash_validation_error
-from flask import render_template, g, request, Blueprint, abort, redirect, url_for, flash
-
-from ..plugins import evaluate_results, evaluate_results_total, report_content
-
+from flask import (
+    render_template,
+    g,
+    Blueprint,
+)
+from ..plugins import report_content
 
 bp = Blueprint("evaluation", __name__, url_prefix="/evaluation")
 
 
 @bp.get("/")
 def index():
-    return render_template(
-        "evaluation.html",
-        report_content=report_content(g.backend)
-    )
+    return render_template("evaluation.html", report_content=report_content(g.backend))
